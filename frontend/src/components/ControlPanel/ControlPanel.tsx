215b7ac725c4e38916a05e05dc34c3236fbdf094
Kushina <superaoba0602@gmail.com>
2026-06-17 14:55:52 +0900

    chore(frontend): VideoPanelとControlPanelの雛形を追加

---
+++ frontend/src/components/ControlPanel/ControlPanel.tsx
@@ -0,0 +1,7 @@
+export function ControlPanel() {
+  return (
+    <section className="control-panel">
+        <h2>Control Panel</h2>
+    </section>
+  );
+}
---
+++ frontend/src/components/VideoPanel/VideoPanel.tsx
@@ -0,0 +1,7 @@
+export function VideoPanel() {
+  return (
+    <section className="video-panel">
+      <h2>Video Panel</h2>
+    </section>
+  );
+}
