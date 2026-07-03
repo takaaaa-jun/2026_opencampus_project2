ac103e81ca6726c45b8ecccdb2951af013b7a338
takaaaa-jun <220669610+takaaaa-jun@users.noreply.github.com>
2026-07-03 09:14:30 +0900

    integrate backend and manim

---
+++ backend/webrtc/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('pose/update/', views.pose_update, name='pose_update'),
+    path('pose/latest/', views.pose_latest, name='pose_latest'),
+    path('image/update/', views.image_update, name='image_update'),
+    path('image/latest/', views.image_latest, name='image_latest'),
+]
